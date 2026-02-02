7071642691699cf85f2528172e701867e17e76cf
xaoccc <xaocccc@gmail.com>
2026-02-02 14:46:56 +0200

    hello java

---
+++ helloworld/App.java
@@ -0,0 +1,7 @@
+package helloworld;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Java!");
+    }
+}
