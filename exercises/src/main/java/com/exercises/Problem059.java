571482665a61f16f6f1f33d0531847ccbf86b01c
xaoccc <xaocccc@gmail.com>
2026-02-24 09:57:55 +0200

    more java to test lang stats workfolw

---
+++ exercises/src/main/java/com/exercises/Problem059.java
@@ -0,0 +1,17 @@
+package com.exercises;
+import java.util.Scanner;
+// Description:
+// Write a program that reads text (string) entered by the user, calculates and prints the sum of the vowel values ​​according to the table below:
+// letter a e i o u
+// value 1 2 3 4 5
+// Solution:
+public class Problem059 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            String text = input.nextLine().strip();
+            for (int i = 0; i < text.length(); i++) {
+                
+            }
+        }
+    }
+}
