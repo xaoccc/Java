2aff0773142dcf6721da0902c6fb5f8a8ef0bbe4
xaoccc <xaocccc@gmail.com>
2026-03-04 10:16:45 +0200

    more java

---
+++ exercises/src/main/java/com/exercises/Problem087.java
@@ -0,0 +1,11 @@
+package com.exercises;
+
+public class Problem087 {
+    public static void main(String[] args) {
+        for (int h=0;h<24;h++) {            
+            for (int m=0;m<60;m++) {
+                System.out.printf("%d:%d\n", h, m);
+            }
+        }
+    }
+}
